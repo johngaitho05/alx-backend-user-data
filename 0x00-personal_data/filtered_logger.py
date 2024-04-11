d2863c6ccb6040e4073427d31a5c3713390224e3
John Gaitho <johngaitho05@gmail.com>
2024-04-11 13:51:06 +0300

    obfuscate

---
+++ 0x00-personal_data/filtered_logger.py
@@ -0,0 +1,10 @@
+import re
+
+import re
+
+
+def filter_datum(fields, redaction, message, separator):
+    """obfuscate a log datum"""
+    return re.sub(
+        f'(?:(?<=^)|(?<={separator}))({"|".join(fields)}){separator}',
+        f'{redaction}{separator}', message)
