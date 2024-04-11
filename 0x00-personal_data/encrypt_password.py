8900b1f6c396c8cdb833479a17357ddbf88f1fe1
John Gaitho <johngaitho05@gmail.com>
2024-04-11 15:34:38 +0300

    password validation

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -24,3 +24,18 @@ def hash_password(password: str) -> bytes:
     salt = bcrypt.gensalt()
     hashed_password = bcrypt.hashpw(password.encode('utf-8'), salt)
     return hashed_password
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """
+    Validates whether the provided password matches the hashed password.
+
+    Args:
+    hashed_password (bytes): The hashed password.
+    password (str): The password to be validated.
+
+    Returns:
+    bool: True if the password matches the hashed password, False otherwise.
+    """
+    # Verify if the provided password matches the hashed password
+    return bcrypt.checkpw(password.encode('utf-8'), hashed_password)
