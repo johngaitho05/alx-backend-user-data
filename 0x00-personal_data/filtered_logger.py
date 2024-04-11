3b445beaed01cf0ad6ae2077d2725976986be600
John Gaitho <johngaitho05@gmail.com>
2024-04-11 14:27:37 +0300

    custom log formatter

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -26,6 +26,7 @@ class RedactingFormatter(logging.Formatter):
     SEPARATOR = ";"
 
     def __init__(self, fields: List[str]) -> None:
+        """Initialize the formatter"""
         super(RedactingFormatter, self).__init__(self.FORMAT)
         self.fields = fields
 
