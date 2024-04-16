49d8963d93faaf2a30778000c56f67be26bf42f9
John Gaitho <johngaitho05@gmail.com>
2024-04-16 16:02:06 +0300

    implement require_auth

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -15,8 +15,12 @@ class Auth:
         pass
 
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
-        """Checks whether authentication is required."""
-        return False
+        """Checks whether authentication is for a given path"""
+        if not path:
+            return True
+        if not excluded_paths:
+            return True
+        return path.rstrip('/') not in [p.rstrip('/') for p in excluded_paths]
 
     def authorization_header(self, request: flask_request = None) -> str:
         """Returns the authorization header of a request"""
