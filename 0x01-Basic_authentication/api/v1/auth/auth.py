7b9ed895194db237d2402e21551d93938d8334c3
John Gaitho <johngaitho05@gmail.com>
2024-04-16 15:44:31 +0300

    define Auth class

---
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+"""
+Request Authorization Template
+"""
+from typing import List, TypeVar
+
+from flask import request as flask_request
+
+
+class Auth:
+    """Auth class"""
+
+    def __init__(self):
+        """Initialization"""
+        pass
+
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """Checks whether authentication is required."""
+        return False
+
+    def authorization_header(self, request: flask_request = None) -> str:
+        """Returns the authorization header of a request"""
+        return
+
+    def current_user(self, request: flask_request = None) -> TypeVar('User'):
+        """Returns the current user."""
+        return
