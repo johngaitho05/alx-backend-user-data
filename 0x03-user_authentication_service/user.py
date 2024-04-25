5b4e0098113201413591ac17130c8cae628c6797
John Gaitho <johngaitho05@gmail.com>
2024-04-25 15:28:55 +0300

    refactor

--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -1,14 +1,13 @@
 #!/usr/bin/env python3
-""" holds class User"""
-
+""" User module """
 from sqlalchemy import Column, String, Integer
-from sqlalchemy.orm import declarative_base
+from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
 
 
 class User(Base):
-    """Representation of a user """
+    """ User class """
     __tablename__ = 'users'
     id = Column(Integer, primary_key=True)
     email = Column(String(250), nullable=False)
